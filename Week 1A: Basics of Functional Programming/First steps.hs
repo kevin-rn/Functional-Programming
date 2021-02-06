823fc235ed9ba100bf393227b40cff154e730ea3
KevinRN <45128598+K-R-N@users.noreply.github.com>
2021-02-06 15:08:06 +0100

    Update First steps.hs

--- Week 1A: Basics of Functional Programming/First steps.hs
+++ Week 1A: Basics of Functional Programming/First steps.hs
@@ -1,8 +1,8 @@
--- Write a function called add_and_double which adds two numbers together and then doubles the result, by replacing undefined with the proper expression. --
+-- Write a function called add_and_double which adds two numbers together and then doubles the result, by replacing undefined with the proper expression.
 
 add_and_double x y = (x + y)*2
 
--- test --
+-- test 
 prop_add_and_double_example :: Property
 prop_add_and_double_example = add_and_double 1 1 === 4
 
