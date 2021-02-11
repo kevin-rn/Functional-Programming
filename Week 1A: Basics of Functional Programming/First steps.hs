837bd57df9e32e456f6effbff449acfdab1aef95
KevinRN <45128598+Kevin-RN@users.noreply.github.com>
2021-02-11 23:24:40 +0100

    Delete Week 1A: Basics of Functional Programming directory

--- Week 1A: Basics of Functional Programming/First steps.hs
+++
@@ -1,9 +0,0 @@
--- Write a function called add_and_double which adds two numbers together and then doubles the result, by replacing undefined with the proper expression.
-add_and_double x y = undefined
-
--- test 
-prop_add_and_double_example :: Property
-prop_add_and_double_example = add_and_double 1 1 === 4
-
--- solution
-add_and_double x y = (x + y)*2
